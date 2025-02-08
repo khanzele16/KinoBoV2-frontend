7c43dcbdd803770374cdaac152e2fdf2f641e28b
webddevel <unar0448@gmail.com>
2025-02-08 17:57:59 +0300

    edited

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ import './App.css'
 import Header from './Components/Header/Header'
 function App() {
   return (
-    <div>
+    <div className='App'>
       <Header />
+      <div className="content">
+        <div className="container">
+          
+        </div>
+      </div>
     </div>
   )
 }
