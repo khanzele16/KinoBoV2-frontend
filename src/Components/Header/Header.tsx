84b5c1d97fc177fb47ab8db9b19eea2a7607899e
khanzele16 <khanzele16@gmail.com>
2025-02-08 17:21:11 +0300

    Edited smth

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,20 +1,16 @@
-import React from 'react'
+import React from "react";
 
 function Header() {
   return (
-    <div className='Header'>
+    <div className="Header">
       <div className="Header-past">
         <div className="Header-left__past">
-            
+          
         </div>
-        <div className="Header-right__past">
-
-        </div>
+        <div className="Header-right__past"></div>
       </div>
-      
-      
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
