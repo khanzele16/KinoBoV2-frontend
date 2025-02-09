7f8336b86a02bcd143b407a9afad851b3c919acf
khanzele16 <khanzele16@gmail.com>
2025-02-09 23:48:07 +0300

    Edited

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,22 +1,16 @@
-import React from 'react'
-import logo from '../../assets/Header/header-logo.png'
-import './Header.css'
+import React from "react";
+import logo from "./../../assets/Header/header-logo.png";
+import "./Header.css";
+
 function Header() {
   return (
     <div className="Header">
-      <div className="Header-past">
-        <div className="Header-left__past">
+      <div className="Header-content">
+        <div className="Header-content-menu">
           
-          <div className="Header-logo">
-            <img src={logo} alt="" />
-          </div>
-        </div>
-        <div className="Header-right__past">
-
+          <img src={logo} alt="" />
         </div>
       </div>
-
-
     </div>
   );
 }
